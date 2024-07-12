15db0f304f3e29713095b87f3033a0c6d3b5949d
oriverk <oriveryk@gmail.com>
2024-07-12 16:36:12 +0900

    feat: add animated_responsive_layout

---
+++ animated_responsive_layout/README.md
@@ -0,0 +1,16 @@
+# animated_responsive_layout
+
+A new Flutter project.
+
+## reference
+
+- [マテリアル 3 でアニメーション化されたレスポンシブ アプリ レイアウトを作成する](https://codelabs.developers.google.com/codelabs/flutter-animated-responsive-layout?hl=ja#0)
+- [codelabs/animated-responsive-layout at main · flutter/codelabs](https://github.com/flutter/codelabs/tree/main/animated-responsive-layout)
+
+<p align="center">
+  <img src="/animated_responsive_layout/images/wide.webp" alt="wide ss" width="700" />
+</p>
+
+<p align="center">
+  <img src="/animated_responsive_layout/images/narrow.webp" alt="narrow ss" width="350"  />
+</p>
---
+++ animated_responsive_layout/lib/widgets/disappearing_navigation_rail.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import '../animations.dart';
+import '../destinations.dart';
+import '../transitions/nav_rail_transition.dart';
+import 'animated_floating_action_button.dart';
+
+class DisappearingNavigationRail extends StatelessWidget {
+  const DisappearingNavigationRail({
+    super.key,
+    required this.railAnimation,
+    required this.railFabAnimation,
+    required this.backgroundColor,
+    required this.selectedIndex,
+    this.onDestinationSelected,
+  });
+
+  final RailAnimation railAnimation;
+  final RailFabAnimation railFabAnimation;
+  final Color backgroundColor;
+  final int selectedIndex;
+  final ValueChanged<int>? onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavRailTransition(
+      animation: railAnimation,
+      backgroundColor: backgroundColor,
+      child: NavigationRail(
+        selectedIndex: selectedIndex,
+        backgroundColor: backgroundColor,
+        onDestinationSelected: onDestinationSelected,
+        leading: Column(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+            ),
+            const SizedBox(height: 8),
+            AnimatedFloatingActionButton(
+              animation: railFabAnimation,
+              elevation: 0,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        groupAlignment: -0.85,
+        destinations: destinations.map((d) {
+          return NavigationRailDestination(
+            icon: Icon(d.icon),
+            label: Text(d.label),
+          );
+        }).toList(),
+      ),
+    );
+
+    // final colorScheme = Theme.of(context).colorScheme;
+    // return NavigationRail(
+    //   selectedIndex: selectedIndex,
+    //   backgroundColor: backgroundColor,
+    //   onDestinationSelected: onDestinationSelected,
+    //   leading: Column(
+    //     children: [
+    //       IconButton(
+    //         onPressed: () {},
+    //         icon: const Icon(Icons.menu),
+    //       ),
+    //       const SizedBox(height: 8),
+    //       FloatingActionButton(
+    //         shape: const RoundedRectangleBorder(
+    //           borderRadius: BorderRadius.all(
+    //             Radius.circular(15),
+    //           ),
+    //         ),
+    //         backgroundColor: colorScheme.tertiaryContainer,
+    //         foregroundColor: colorScheme.onTertiaryContainer,
+    //         onPressed: () {},
+    //         child: const Icon(Icons.add),
+    //       ),
+    //     ],
+    //   ),
+    //   groupAlignment: -0.85,
+    //   destinations: destinations.map((d) {
+    //     return NavigationRailDestination(
+    //       icon: Icon(d.icon),
+    //       label: Text(d.label),
+    //     );
+    //   }).toList(),
+    // );
+  }
+}
